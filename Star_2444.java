71bf2b9a8585d762edb82904741ea279a8ce4843
ryann1203 <130066603+ryann1203@users.noreply.github.com>
2023-10-09 12:27:28 +0900

    Update Star_2444.java

--- Star_2444.java
+++ Star_2444.java
@@ -8,7 +8,7 @@ class Main {
 
       for(int i = 1; i <= n; i++){
 
-        for(int j = 0; j < n - 1; j++){ //공백 출력
+        for(int j = 0; j < n - i; j++){ //공백 출력
           System.out.print(" ");
         }
 
@@ -21,7 +21,7 @@ class Main {
 
       for(int i = n - 1; i >= 0; i--){
 
-        for(int j = 0; j < n - 1; j++){ //공백 출력
+        for(int j = 0; j < n - i; j++){ //공백 출력
           System.out.print(" ");
         }
 
